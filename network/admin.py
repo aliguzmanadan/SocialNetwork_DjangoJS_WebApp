cb89e81a13a54cbbc307c2f2eaabd75993c90c65
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-12-22 18:21:29 +0100

    Creted models and superuser

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Post
 
 # Register your models here.
+class User_Admin(admin.ModelAdmin):
+    list_display = ("username", "email")
+
+class Post_Admin(admin.ModelAdmin):
+    list_display = ("poster", "content", "timestamp")
+
+admin.site.register(User, User_Admin)
+admin.site.register(Post, Post_Admin)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import TextField
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = ManyToManyField(User, blank=True, related_name="liked_posts")
