9e59318e38a85339b18fd4d84aabf84e975865cb
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-12-23 14:04:25 +0100

    Showing posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -15,6 +15,44 @@ document.addEventListener('DOMContentLoaded', function() {
 function load_posts(posts_set){
     document.querySelector('#session_name').innerHTML = `<h3>${posts_set.charAt(0).toUpperCase() + posts_set.slice(1)}</h3>`;
 
+    //Getting posts
+    fetch(`/posts/${posts_set}`)
+    .then(response => response.json())
+    .then(posts => {
+        // Print posts in cosole
+        console.log(posts);
+
+        //display each post
+        posts.forEach(post => display_post(post));
+});
+}
+
+////////////////
+
+function display_post(post){
+
+    //Creating div for each post
+    let div_post = document.createElement('div');
+    div_post.className = "card";
+
+    let body = document.createElement('div');
+    body.className = "card-body";
+    body.innerHTML = `<h5 class="card-title"> ${post.poster} </h5>`
+
+    let content = document.createElement('p');
+    content.className = "card-text";
+    content.innerHTML = post.content;
+
+    let timestamp = document.createElement('p');
+    timestamp.className = "card-text";
+    timestamp.innerHTML = `<small class="text-muted">${post.timestamp}</small>`
+
+    //Assemble post
+    body.append(content, timestamp);
+    div_post.append(body);
+    document.querySelector('#session_posts').append(div_post);
+
+
 }
 
 //////////////////
